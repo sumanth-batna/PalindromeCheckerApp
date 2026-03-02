5c16927b7b0709a89996bf6e4dd01f2ccf3fe8dd
sumanth_batna <sb1000@srmist.edu.in>
2026-03-02 16:51:24 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -18,3 +18,26 @@ public class PalindromeCheckerApp {
         System.out.println("Ready to proceed to palindrome validation...");
     }
 }
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+
+        String word = "madam";
+
+
+        String reversed = "";
+
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+    }
+}
